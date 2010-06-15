2b6d0198b4ca58bcd25b875565dc85d3a507de0b
Peter Bengtsson <peter@fry-it.com>
2010-06-15 15:59:01 -0400

    added django_wsgi.py, uwsgi.xml; modified settings.py

--- settings.py
+++ settings.py
@@ -6,16 +6,20 @@ TEMPLATE_DEBUG = DEBUG
 import os
 HERE = os.path.normpath(os.path.dirname(__file__))
 
+EMAIL_HOST = 'mail.fry-it.com'
+EMAIL_SUBJECT_PREFIX = '[TfL Cameras] '
+SERVER_EMAIL = 'django@peterbe.fry-it.com'
+
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+     ('Peter', 'mail@peterbe.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tflcameras',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'tflcameras.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
